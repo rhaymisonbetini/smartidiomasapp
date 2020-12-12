a5c4c146bf4665137c05612f6c165f4ffbea13ec
RHAYMISON <rhaymison@hotmail.com>
2020-12-12 11:46:05 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { NavController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -13,15 +13,35 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private navController: NavController
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+
+      const token = localStorage.getItem("incidente_token");
+      const user = localStorage.getItem("email");
+
+      if (token && user) {
+
+        this.statusBar.backgroundColorByHexString('#5260ff');
+        this.statusBar.styleLightContent();
+        this.splashScreen.hide();
+        this.navController.navigateRoot('')
+
+      } else {
+        this.statusBar.backgroundColorByHexString('#5260ff');
+        this.statusBar.styleLightContent();
+        this.splashScreen.hide();
+        this.navController.navigateRoot('/login')
+      }
+
+      // this.statusBar.styleLightContent();
+      // this.splashScreen.hide();
+
     });
   }
 }
